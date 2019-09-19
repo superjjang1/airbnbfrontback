1fdf2463296a12c1c989820b50fd38759bae525c
Sean Yim <superjjang89@gmail.com>
2019-09-19 14:15:32 -0400

    react-router-dom, import browser router as router, route

--- airbnb-front-end/src/App.js
+++ airbnb-front-end/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <h1>Sanity check</h1>
     </div>
+
+    </Router>
   );
 }
 
