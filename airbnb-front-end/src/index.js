9b2851a1715e0ea46a93b7cb0c40cdf9bd230d23
Sean Yim <superjjang89@gmail.com>
2019-09-19 14:01:36 -0400

    bootstrapping reducers, and other modules

--- airbnb-front-end/src/index.js
+++ airbnb-front-end/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from './reducer/rootReducer';
 
 ReactDOM.render(
     <App />, 
