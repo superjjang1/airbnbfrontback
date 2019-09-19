6d3549b3e98e37b8114905aa81acb0926bb9433b
Sean Yim <superjjang89@gmail.com>
2019-09-19 14:10:23 -0400

    redux promise, provider, store, middleware, the store

--- airbnb-front-end/src/index.js
+++ airbnb-front-end/src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reduxPromise from 'redux-promise';
+
 import { Provider } from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
-import rootReducer from './reducer/rootReducer';
+import rootReducer from './reducers/rootReducer';
+
+const theStore = applyMiddleware(reduxPromise)(createStore)(rootReducer);
+
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={theStore}>
+        <App />
+
+    </Provider>,
     document.getElementById('root')
 );
 
