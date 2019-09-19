1c3d419b99c0bc744f530c090592d15068a60551
Sean Yim <superjjang89@gmail.com>
2019-09-19 14:32:27 -0400

    more skeletonning

--- airbnb-front-end/src/components/NavBar/NavBar.js
+++ airbnb-front-end/src/components/NavBar/NavBar.js
@@ -9,11 +9,25 @@ class NavBar extends Component {
         return ( 
         <div className="container-fluid nav">
             <div className="row">
-                <div className="black">
+                <nav className="black">
                     <div className="nav-wrapper">
                         <Link to="/" className="brand-logo">AirBnB</Link>
+                        <ul id="nav-mobile" className="right">
+                            <li className="nav-item">
+                                <Link to=""></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to=""></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to=""></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to=""></Link>
+                            </li>
+                        </ul>
                     </div>
-                </div>
+                </nav>
             </div>
         </div>
          );
