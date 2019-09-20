1255e024343cbf2f699916b87a29c45d7efe3606
Sean Yim <superjjang89@gmail.com>
2019-09-20 13:51:44 -0400

    making a modal, and being able to close it

--- airbnb-front-end/src/components/NavBar/NavBar.js
+++ airbnb-front-end/src/components/NavBar/NavBar.js
@@ -8,7 +8,14 @@ class NavBar extends Component {
         showModal : false
     }
     signup = (e) =>{
-
+        this.setState({
+            showModal: true
+        })
+    }
+    closeModal = (e) => {
+        this.setState({
+            showModal: false
+        })
     }
 
     render() { 
@@ -28,16 +35,17 @@ class NavBar extends Component {
                             <li>
                                 <Link to="/help">Help</Link>
                             </li>
-                            <li onClick={this.signup}>
+                            <li className="nav-non-link" onClick={this.signup}>
                                 Sign up
                             </li>
-                            <li onClick={this.signup}>
+                            <li className="nav-non-link" onClick={this.signup}>
                                Log in
                             </li>
                         </ul>
                     </div>
                 </nav>
-                <div className="login-modal">
+                <div className="login-modal" style={this.state.showModal ? {"display":"block"} : {}}>
+                    <button className="close-modal" onClick={this.closeModal}>x</button>
                     <h1>Modal</h1>
 
                 </div>
