22c3c44c434d73d27c7c74d094d47e681b52e787
Sean Yim <superjjang89@gmail.com>
2019-09-23 11:20:50 -0400

    login email signup events

--- airbnb-front-end/src/components/NavBar/Login.js
+++ airbnb-front-end/src/components/NavBar/Login.js
@@ -1,19 +1,33 @@
 import React from 'react';
 
 
-function Login(props){
-    return(
-        <div className="login-form">
-        <button className="facebook-login">Connect With Facebook</button>
-        <button className="google-login">Connect with Google</button>
-        <span>or</span>
-        <input className="email-signup" placeholder="Email address" />
-        <input className="password-signup" placeholder="Password" />
-        <button className="sign-up-button">Log In</button>
-        <div className="border-rule"></div>
-        <div className="login-text align-left">Don't have an account? <span onClick={()=>{props.changeModalContent('signup')}} >Sign Up</span></div>
-        </div>
-    )
+class Login extends React.Component{
+    state ={
+        email: "",
+        password:""
+    }
+    changeEmail = (e) => {
+        this.setState({email: e.target.value})
+    }
+    changePass = (e) => {
+        this.setState({password: e.target.value})
+    }
+
+    render(){
+        return(
+            <div className="login-form">
+            <button className="facebook-login">Connect With Facebook</button>
+            <button className="google-login">Connect with Google</button>
+            <span>or</span>
+            <input onChange={this.changeEmail} value={this.state.email} className="email-signup" placeholder="Email address" />
+            <input onChange={this.changePass} value={this.state.password} className="password-signup" placeholder="Password" type="password"/>
+            <button className="sign-up-button">Log In</button>
+            <div className="border-rule"></div>
+            <div className="login-text align-left">Don't have an account? <span onClick={()=>{this.props.changeModalContent('signup')}} >Sign Up</span></div>
+            </div>
+        )
+
+    }
 }
 
 export default Login;
--- airbnb-front-end/src/components/NavBar/Signup.js
+++ airbnb-front-end/src/components/NavBar/Signup.js
@@ -1,18 +1,40 @@
 import React from 'react';
 
 
-function SignUp(props){
-    return(
-        <div className="register-form">
-        <input className="email-signup" placeholder="Email address" />
-        <input className="first-signup" placeholder="First name" />
-        <input className="last-signup" placeholder="Last name" />
-        <input className="password-signup" placeholder="Password" />
-        <button className="sign-up-button">Sign up</button>
-        <div className="border-rule"></div>
-        <div className="login-text align-left">Already have an Airbnb account? <span onClick={()=>{props.changeModalContent('login')}}>Log in</span></div>
-        </div> 
-    )
+class SignUp extends React.Component{
+    state ={
+        email: "",
+        first: "",
+        last: "",
+        password:""
+    }
+    changeEmail = (e) => {
+        this.setState({email: e.target.value})
+    }
+    changeFirst = (e) => {
+        this.setState({first: e.target.value})
+    }
+    changeLast = (e) => {
+        this.setState({last: e.target.value})
+    }
+    changePass = (e) => {
+        this.setState({password: e.target.value})
+    }
+
+    render(){
+
+        return(
+            <div className="register-form">
+            <input onChange={this.changeEmail} value={this.state.email} className="email-signup" placeholder="Email address" />
+            <input onChange={this.changeFirst} value={this.state.first} className="first-signup" placeholder="First name" />
+            <input onChange={this.changeLast} value={this.state.last} className="last-signup" placeholder="Last name" />
+            <input onChange={this.changePass} value={this.state.password} className="password-signup" placeholder="Password" type="password" />
+            <button className="sign-up-button">Sign up</button>
+            <div className="border-rule"></div>
+            <div className="login-text align-left">Already have an Airbnb account? <span onClick={()=>{this.props.changeModalContent('login')}}>Log in</span></div>
+            </div> 
+        )
+    }
 }
 
 export default SignUp;
