e83bf08820272b88f41a3fbc5ea5b9352c94d650
Sean Yim <superjjang89@gmail.com>
2019-09-25 15:35:35 -0400

    ability to upload images

--- airbnb-back-end/routes/host.js
+++ airbnb-back-end/routes/host.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../db');
+var multer = require('multer');
+var upload = multer({dest: './public/images/'});
 
 //this triggers when post('/host/homes') only applies in /host.
 
-router.post('/homes',(req,res)=>{
+router.post('/homes',upload.single('locationImage'),(req,res)=>{
     console.log(req.body);
+    console.log(req.file);
     res.json(req.body);
 });
 
