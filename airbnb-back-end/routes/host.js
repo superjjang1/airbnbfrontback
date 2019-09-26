2c92e6ca1b344ca76d531eaddce6a123b020af33
Sean Yim <superjjang89@gmail.com>
2019-09-26 09:12:46 -0400

    made the upload work

--- airbnb-back-end/routes/host.js
+++ airbnb-back-end/routes/host.js
@@ -3,12 +3,18 @@ var router = express.Router();
 const db = require('../db');
 var multer = require('multer');
 var upload = multer({dest: './public/images/'});
+const fs = require('fs');
 
 //this triggers when post('/host/homes') only applies in /host.
 
 router.post('/homes',upload.single('locationImage'),(req,res)=>{
     console.log(req.body);
     console.log(req.file);
+    //rename filepath
+    const f = req.file;
+    fs.rename(f.path, f.destination+'/'+f.originalname,(err)=>{
+        if(err)throw err;
+    })
     res.json(req.body);
 });
 
